42ca8f483913442ed6e60e0208fe92ea6e5fde8e
Matthew Pierce <lesbianhaircut@outlook.com>
2021-09-12 09:26:33 -0400

    c

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
 import { data } from "./data.js";
 
-window.onload = () => {};
+window.onload = () => {
+  // Variables
+  const curHours = document.querySelectorAll(".hours-current");
+  const prevHours = document.querySelectorAll(".hours-previous");
+  const titles = document.querySelectorAll(".title");
+  const daily = document.querySelector("#daily");
+  const weekly = document.querySelector("#weekly");
+  const monthly = document.querySelector("#monthly");
+
+  data.forEach((item) => {
+    console.log(item);
+  });
+
+  (function () {
+    titles.forEach((title, index) => {
+      title.innerText = data[index].title;
+    });
+  })();
+
+  // Functions
+  function setTimeFrame(timeframe) {
+    curHours.forEach((hours, index) => {
+      hours.innerText = data[index].timeframes[timeframe].current;
+    });
+    prevHours.forEach((hours, index) => {
+      hours.innerText = `Last Week - ${data[index].timeframes[timeframe].previous}hrs`;
+    });
+  }
+
+  setTimeFrame("monthly");
+
+  // Event listeners
+
+  daily.addEventListener("click", () => {
+    setTimeFrame("daily");
+  });
+
+  weekly.addEventListener("click", () => {
+    setTimeFrame("weekly");
+  });
+
+  monthly.addEventListener("click", () => {
+    setTimeFrame("monthly");
+  });
+};
